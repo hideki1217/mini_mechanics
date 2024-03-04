41728a69e8ef697016a035c2240e8b7e8ff49d9e
HidekiOkumura <okumurahidekikangeki@gmail.com>
2024-03-04 23:26:24 +0900

    impl eye camera

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    f32::consts::PI,
     ops::{Add, Div, Mul, Neg, Sub},
     thread::sleep,
     time::{Duration, Instant},
@@ -280,63 +281,60 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 center: Vec3(1., -3., -6.),
                 verosity: -Vec3::ones(),
             },
-            Sphere {
-                weight: 1.,
-                rgb: RGB::red(),
-                radius: 1.,
-                center: Vec3(10., -2., -6.),
-                verosity: -Vec3::ones(),
-            },
-            Sphere {
-                weight: 1.,
-                rgb: RGB::red(),
-                radius: 1.,
-                center: Vec3(15., -3., -6.),
-                verosity: -Vec3::ones(),
-            },
-            Sphere {
-                weight: 1.,
-                rgb: RGB::red(),
-                radius: 1.,
-                center: Vec3(2., -4., -2.),
-                verosity: -Vec3::ones(),
-            },
         ],
         vector_field: |x: &Vec3| -x * 5.,
     };
     let render = |world: &World, pixel_data: &mut [u8]| {
-        const CAMERA_POSITION: Vec3 = Vec3(0., 0., 10.);
+        const CAMERA_POSITION: Vec3 = Vec3(0., 0., 20.);
         const X_DIRECTION: Vec3 = Vec3(1., 0., 0.);
         const Y_DIRECTION: Vec3 = Vec3(0., 1., 0.);
         const Z_DIRECTION: Vec3 = Vec3(0., 0., -1.);
-        const PPM: f32 = 20.;
-        const WIDTH_M: f32 = WIDTH as f32 / PPM;
-        const HEIGHT_M: f32 = HEIGHT as f32 / PPM;
+
+        const X_EYE_ANGLE: f32 = PI / 8.;
+        const EYE_CELL_ANGLE: f32 = X_EYE_ANGLE / WIDTH as f32;
+        const Y_EYE_ANGLE: f32 = EYE_CELL_ANGLE * HEIGHT as f32;
+        const EYE_RADIUS: f32 = 0.3;
+        let eye_center: Vec3 = CAMERA_POSITION + Z_DIRECTION * EYE_RADIUS;
 
         for x in 0..WIDTH {
             for y in 0..HEIGHT {
-                let pos = CAMERA_POSITION
-                    + X_DIRECTION * (x as f32 / PPM - WIDTH_M / 2.)
-                    + Y_DIRECTION * (y as f32 / PPM - HEIGHT_M / 2.);
-                let n = Z_DIRECTION;
+                let camera_pos = CAMERA_POSITION;
+                let n = {
+                    let x_angle = EYE_CELL_ANGLE * x as f32 - X_EYE_ANGLE / 2.;
+                    let y_angle = EYE_CELL_ANGLE * y as f32 - Y_EYE_ANGLE / 2.;
+                    let eye_pos =
+                        eye_center + X_DIRECTION * x_angle.sin() + Y_DIRECTION * y_angle.sin();
+                    let v = eye_pos - camera_pos;
+                    v / v.norm()
+                };
 
-                let mut min_dist: Option<f32> = None;
+                let mut visible_obj: Option<(f32, &Sphere)> = None;
                 for object in world.objects() {
-                    let (e, d) = {
-                        let v = object.center - pos;
-                        let dist = v.norm();
-                        (v / dist, dist)
+                    let col_pos = {
+                        let (e, d_e) = {
+                            let v = object.center - camera_pos;
+                            let dist = v.norm();
+                            (v / dist, dist)
+                        };
+
+                        let cos = e.inner(&n);
+                        let sin = (1. - cos.powi(2)).sqrt();
+                        if cos >= 0. && (d_e * sin) <= object.radius {
+                            let d_n =
+                                d_e * cos - (object.radius.powi(2) - (d_e * sin).powi(2)).sqrt();
+                            Some(camera_pos + n * d_n)
+                        } else {
+                            None
+                        }
                     };
-                    let cos = e.inner(&n);
-                    let sin = (1. - cos.powi(2)).sqrt();
-                    if cos >= 0. && (d * sin) <= object.radius {
-                        let d = d * cos - (object.radius.powi(2) - (d * sin).powi(2)).sqrt();
-                        match min_dist {
-                            Some(min) if min > d => {
-                                min_dist = Some(d);
+                    if let Some(col_pos) = col_pos {
+                        let d = (col_pos - camera_pos).norm();
+                        match visible_obj {
+                            Some((min_dist, _)) if min_dist > d => {
+                                visible_obj = Some((d, object));
                             }
                             None => {
-                                min_dist = Some(d);
+                                visible_obj = Some((d, object));
                             }
                             _ => {}
                         }
@@ -344,11 +342,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 }
 
                 let base: usize = (x + y * WIDTH) * 3;
-                if let Some(d) = min_dist {
-                    let point = pos + n * d;
-                    pixel_data[base + 0] = (point.2 * 30. + 128.).min(255.).max(1.).floor() as u8;
-                    pixel_data[base + 1] = (point.2 * 30. + 128.).min(255.).max(1.).floor() as u8;
-                    pixel_data[base + 2] = (point.2 * 30. + 128.).min(255.).max(1.).floor() as u8;
+                if let Some((d, obj)) = visible_obj {
+                    let brawer = CAMERA_POSITION.norm().powi(2) / 1.4 / d.powi(2);
+                    pixel_data[base + 0] = (obj.rgb.0 as f32 * brawer).max(1.).min(255.) as u8;
+                    pixel_data[base + 1] = (obj.rgb.1 as f32 * brawer).max(1.).min(255.) as u8;
+                    pixel_data[base + 2] = (obj.rgb.2 as f32 * brawer).max(1.).min(255.) as u8;
                 } else {
                     pixel_data[base + 0] = 0;
                     pixel_data[base + 1] = 0;
